97c9045e5c7297ea1cf3b8330ae4a47337ce6112
Deepchand Pati <deepchand.pati@oup.com>
2024-05-24 12:01:42 +0530

    video tag isue fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,18 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <ReactPlayer url='https://mm-chat-history-bucket.s3.amazonaws.com/Addition_and_subtraction_problems.mp4' />
+      <video width={800} height={500} controls loop autoPlay muted>
+        <source src='https://d3ez5y0cidphb1.cloudfront.net/Addition_and_subtraction_problems.mp4'
+        type='video/mp4'/>
+      </video>
+      <ReactPlayer url='https://d3ez5y0cidphb1.cloudfront.net/Addition_and_subtraction_problems.mp4'
+
+      controls = {true}
+      />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <video preload="auto" src="https://mm-chat-history-bucket.s3.amazonaws.com/Addition_and_subtraction_problems.mp4"></video>
+
     </>
   )
 }
